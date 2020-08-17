32e5233d19d50e0b4ec8796e470b13e140e24ec0
YuHuo <40259428+18357451081@users.noreply.github.com>
2020-08-17 20:34:39 +0800

    恢复sockboom签到
    恢复签到,并且添加异常捕获

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # 运行Action入口
 import os
+# 捕获异常信息
+import traceback
 
 from sendEmail import SendEmail
 from signin import NeteaseSignin
@@ -12,15 +14,23 @@ if __name__ == '__main__':
     token_url = os.environ['SOCKBOOM_TOKEN']
 
     # 网易云音乐签到
-    net_sign = NeteaseSignin(username=username, password=password)
-    Netease_msg = net_sign.run()
-
+    try:
+        net_sign = NeteaseSignin(username=username, password=password)
+        Netease_msg = net_sign.run()
+    except Exception:
+        print('网易云音乐签到 异常')
+        traceback.print_exc()
+        
     # SockBoom签到
-    # sockboom = SockBoom(token_url=token_url)
-    # SockBoom_msg = sockboom.get_response_msg()
+    try:
+        sockboom = SockBoom(token_url=token_url)
+        SockBoom_msg = sockboom.get_response_msg()
+    except Exception:
+        print('SockBoom签到 异常')
+        traceback.print_exc()
 
-    # context = '网易云签到结果如下:\n' + Netease_msg + '\nSockBoom签到结果如下:\n' + SockBoom_msg
-    context = '网易云签到结果如下:\n' + Netease_msg
+    context = '网易云签到结果如下:\n' + Netease_msg + '\nSockBoom签到结果如下:\n' + SockBoom_msg
+    # context = '网易云签到结果如下:\n' + Netease_msg
     # 发送邮件
     s = SendEmail()
     s.setContent(subject='尊敬的用户您好! ', body=context)
