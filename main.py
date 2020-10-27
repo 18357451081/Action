256c1c797377648f2b2fcb6fc37795679fd3ac3c
YuHuo <40259428+18357451081@users.noreply.github.com>
2020-10-27 22:37:55 +0800

    禁用网易云签到

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ from sock_sign import SockBoom
 
 if __name__ == '__main__':
     # 获取外部变量
-    username = os.environ['NETEASE_USERNAME']
-    password = os.environ['NETEASE_PASSWORD']
+    # username = os.environ['NETEASE_USERNAME']
+    # password = os.environ['NETEASE_PASSWORD']
     token_url = os.environ['SOCKBOOM_TOKEN']
 
     # 网易云音乐签到
-    try:
-        net_sign = NeteaseSignin(username=username, password=password)
-        Netease_msg = net_sign.run()
-    except Exception:
-        print('网易云音乐签到 异常')
-        traceback.print_exc()
+    # try:
+    #     net_sign = NeteaseSignin(username=username, password=password)
+    #     Netease_msg = net_sign.run()
+    # except Exception:
+    #     print('网易云音乐签到 异常')
+    #     traceback.print_exc()
         
     # SockBoom签到
     try:
@@ -29,7 +29,8 @@ if __name__ == '__main__':
         print('SockBoom签到 异常')
         traceback.print_exc()
 
-    context = '网易云签到结果如下:\n' + Netease_msg + '\nSockBoom签到结果如下:\n' + SockBoom_msg
+    context ='\nSockBoom签到结果如下:\n' + SockBoom_msg
+    # context = '网易云签到结果如下:\n' + Netease_msg + '\nSockBoom签到结果如下:\n' + SockBoom_msg
     # context = '网易云签到结果如下:\n' + Netease_msg
     # 发送邮件
     s = SendEmail()
